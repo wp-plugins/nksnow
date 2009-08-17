a0a0fcc55ed219a99685c8f4934ef21ab9fd94f0
nkuttler <nkuttler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-17 19:09:38 +0000

    Update footer message
    git-svn-id: https://plugins.svn.wordpress.org/nksnow/trunk@146740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nksnow.php
+++ nksnow.php
@@ -324,12 +324,15 @@ function nksnow_footer() {
 function nksnow_homelink() {
 	if (
 			!(get_option('nksnow_homelink') === 'Yes')
-	) { ?>
-		<a href="http://www.nkuttler.de/nksnow/">Wordpress snowstorm</a>
-		powered by
-		<a href="http://www.nkuttler.de/nksnow/">nksnow</a>
-		<br />
-<?php
+	) {
+		if (get_option('nksnow_invert') === 'Yes') { ?>
+			<a href="http://www.nkuttler.de/nksnow/">Wordpress balloons</a> <?php
+		} else ?>
+			<a href="http://www.nkuttler.de/nksnow/">Wordpress snowstorm</a>
+			powered by
+			<a href="http://www.nkuttler.de/nksnow/">nksnow</a>
+			<br /> <?php
+		}
 	}
 }
 
